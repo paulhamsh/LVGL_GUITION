2ae7fcf8b694be5c6cd3847bfe1b0029a234dd57
Paul Hamshere <p.w.hamshere@gmail.com>
2024-06-26 17:24:25 +0100

    Create LVGL_GUITION_v8.ino

---
+++ v8/LVGL_GUITION_v8.ino
@@ -0,0 +1,163 @@
+/*******************************************************************************
+ * LVGL Widgets
+ * This is a widgets demo for LVGL - Light and Versatile Graphics Library
+ * import from: https://github.com/lvgl/lv_demos.git
+ *
+ * Dependent libraries:
+ * LVGL: https://github.com/lvgl/lvgl.git
+
+ * Touch libraries:
+ * GT911: https://github.com/TAMCTec/gt911-arduino.git
+ *
+ * LVGL Configuration file:
+ * Copy your_arduino_path/libraries/lvgl/lv_conf_template.h
+ * to your_arduino_path/libraries/lv_conf.h
+ * Then find and set:
+ * #define LV_COLOR_DEPTH     16
+ * #define LV_TICK_CUSTOM     1
+ *
+ * For SPI display set color swap can be faster, parallel screen don't set!
+ * #define LV_COLOR_16_SWAP   1
+ *
+ * Optional: Show CPU usage and FPS count
+ * #define LV_USE_PERF_MONITOR 1
+ ******************************************************************************/
+
+
+#include <lvgl.h>
+#include <demos/lv_demos.h>
+
+#include <Arduino_GFX_Library.h>
+
+#define GFX_BL 38
+
+Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
+  39 /* CS */, 48 /* SCK */, 47 /* SDA */,
+  18 /* DE */, 17 /* VSYNC */, 16 /* HSYNC */, 21 /* PCLK */,
+  11 /* R0 */, 12 /* R1 */, 13 /* R2 */, 14 /* R3 */, 0 /* R4 */,
+  8 /* G0 */, 20 /* G1 */, 3 /* G2 */, 46 /* G3 */, 9 /* G4 */, 10 /* G5 */,
+  4 /* B0 */, 5 /* B1 */, 6 /* B2 */, 7 /* B3 */, 15 /* B4 */
+);
+    Arduino_ST7701_RGBPanel *gfx = new Arduino_ST7701_RGBPanel(
+    bus, GFX_NOT_DEFINED /* RST */, 0 /* rotation */,
+    true /* IPS */, 480 /* width */, 480 /* height */,
+    st7701_type1_init_operations, sizeof(st7701_type1_init_operations),     true /* BGR */,
+    10 /* hsync_front_porch */, 8 /* hsync_pulse_width */, 50 /* hsync_back_porch */,
+    10 /* vsync_front_porch */, 8 /* vsync_pulse_width */, 20 /* vsync_back_porch */);
+
+
+/*******************************************************************************
+ * Please config the touch panel in touch.h
+ ******************************************************************************/
+#include "touch.h"
+
+/* Change to your screen resolution */
+static uint32_t screenWidth;
+static uint32_t screenHeight;
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t *disp_draw_buf;
+static lv_disp_drv_t disp_drv;
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+#if (LV_COLOR_16_SWAP != 0)
+  gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#else
+  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#endif
+
+  lv_disp_flush_ready(disp);
+}
+
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+  if (touch_has_signal())
+  {
+    if (touch_touched())
+    {
+      data->state = LV_INDEV_STATE_PR;
+
+      /*Set the coordinates*/
+      data->point.x = touch_last_x;
+      data->point.y = touch_last_y;
+    }
+    else if (touch_released())
+    {
+      data->state = LV_INDEV_STATE_REL;
+    }
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_REL;
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  // while (!Serial);
+  Serial.println("LVGL Widgets Demo");
+
+  // Init touch device
+  touch_init();
+
+  // Init Display
+  //gfx->begin();
+  
+  gfx->begin(16000000); /* specify data bus speed */
+
+  gfx->fillScreen(BLACK);
+
+#ifdef GFX_BL
+  pinMode(GFX_BL, OUTPUT);
+  digitalWrite(GFX_BL, HIGH);
+#endif
+
+  lv_init();
+
+  screenWidth = gfx->width();
+  screenHeight = gfx->height();
+#ifdef ESP32
+  disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * 200, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+#else
+  disp_draw_buf = (lv_color_t *)malloc(sizeof(lv_color_t) * screenWidth * 200);
+#endif
+  if (!disp_draw_buf)
+  {
+    Serial.println("LVGL disp_draw_buf allocate failed!");
+  }
+  else
+  {
+    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * 200);
+
+    /* Initialize the display */
+    lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    /* Initialize the (dummy) input device driver */
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register(&indev_drv);
+
+    lv_demo_widgets();
+
+    Serial.println("Setup done");
+  }
+}
+
+void loop()
+{
+  lv_timer_handler(); /* let the GUI do its work */
+  delay(5);
+}
