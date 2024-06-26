6f92abb2142a5c10d87488eac1821fe301dbefb0
Paul Hamshere <p.w.hamshere@gmail.com>
2024-06-26 17:48:28 +0100

    Update touch.h

--- v8/touch.h
+++ v8/touch.h
@@ -1,7 +1,9 @@
 /*******************************************************************************
- * GT911: https://github.com/TAMCTec/gt911-arduino.git
- *******************************************************************************/
+ * Touch libraries:
+  * GT911: https://github.com/TAMCTec/gt911-arduino.git
+ ******************************************************************************/
 
+/* uncomment for GT911 */
 #define TOUCH_GT911
 #define TOUCH_GT911_SCL 45
 #define TOUCH_GT911_SDA 19
@@ -22,9 +24,15 @@ TAMC_GT911 ts = TAMC_GT911(TOUCH_GT911_SDA, TOUCH_GT911_SCL, TOUCH_GT911_INT, TO
 
 void touch_init()
 {
+
   Wire.begin(TOUCH_GT911_SDA, TOUCH_GT911_SCL);
   ts.begin();
   ts.setRotation(TOUCH_GT911_ROTATION);
+}
+
+bool touch_has_signal()
+{
+  return true;
 }
 
 bool touch_touched()
@@ -45,11 +53,6 @@ bool touch_touched()
   {
     return false;
   }
-}
-
-bool touch_has_signal()
-{
-  return true;
 }
 
 bool touch_released()
